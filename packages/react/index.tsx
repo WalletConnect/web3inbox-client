84a358133cf7412905d6c6e6d455388c64d5ad1f
Celine Sarafa <celinesarafa@gmail.com>
2023-08-22 11:01:34 +0300

    Specify exports

--- packages/react/index.tsx
+++ packages/react/index.tsx
@@ -5,5 +5,5 @@ export { default as W3iWidget } from "./src/W3iWidget";
 export { default as W3iContext } from "./src/W3iContext";
 export { default as W3iButton } from "./src/W3iButton";
 
-export * from "./src/hooks/widgetManagementHooks";
-export * from "./src/hooks/accountManagementHooks";
+export { useManageW3iWidget } from "./src/hooks/widgetManagementHooks";
+export { useIsSubscribed } from "./src/hooks/accountManagementHooks";
