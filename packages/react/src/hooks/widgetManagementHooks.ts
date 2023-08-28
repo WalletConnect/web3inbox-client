519a2b8d90b0ab2d67a89d31c232c965949d72da
Celine Sarafa <celinesarafa@gmail.com>
2023-08-28 12:03:07 +0300

    Expose load and isopen state

--- packages/react/src/hooks/widgetManagementHooks.ts
+++ packages/react/src/hooks/widgetManagementHooks.ts
@@ -1,25 +1,35 @@
-import { toggleW3iWidget, watchWidgetVisibility } from "@web3inbox/widget-html";
+import {
+  toggleW3iWidget,
+  watchWidgetLoad,
+  watchWidgetVisibility,
+} from "@web3inbox/widget-html";
 import {
   closeW3iWidget,
   openW3iWidget,
   w3iWidgetIsOpen,
+  w3iWidgetIsLoaded,
 } from "@web3inbox/widget-html";
 import { useEffect, useState } from "react";
 
 export const useManageW3iWidget = () => {
-  const [isVisible, setIsVisible] = useState(w3iWidgetIsOpen);
+  const [isOpen, setIsOpen] = useState(w3iWidgetIsOpen);
+  const [isLoaded, setIsLoaded] = useState(w3iWidgetIsLoaded);
 
   useEffect(() => {
-    const sub = watchWidgetVisibility(setIsVisible);
+    const visibilitySub = watchWidgetVisibility(setIsOpen);
+    const loadSub = watchWidgetLoad(setIsLoaded);
+
     return () => {
-      sub.unsubscribe();
+      visibilitySub.unsubscribe();
+      loadSub.unsubscribe();
     };
-  }, [setIsVisible]);
+  }, [setIsOpen]);
 
   return {
     open: openW3iWidget,
     close: closeW3iWidget,
     toggle: toggleW3iWidget,
-    isOpen: isVisible,
+    isOpen,
+    isLoaded,
   };
 };
--- packages/ui/src/utils/events.ts
+++ packages/ui/src/utils/events.ts
@@ -1,9 +1,15 @@
 import { BehaviorSubject } from "rxjs";
 
+// Is Visible?
 export const widgetVisibilitySubject = new BehaviorSubject(false);
 
+// Is Loaded?
+export const widgetLoadSubject = new BehaviorSubject(false);
+
+// Did get a notification?
 export const widgetRecentNotificationsSubject = new BehaviorSubject(0);
 
+// Is the account passed subscribed?
 export const widgetAccountSubject = new BehaviorSubject({
   isSubscribed: false,
 });
