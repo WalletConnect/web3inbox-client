1523bf88b156ed0c72e357e79d69ae7035049511
enesozturk <enesozturk.d@gmail.com>
2024-01-23 12:45:26 +0300

    refactor: update packages

--- packages/react/tsup.config.ts
+++ packages/react/tsup.config.ts
@@ -1,17 +1,9 @@
-import { sassPlugin } from "esbuild-sass-plugin";
 import { defineConfig, Options } from "tsup";
 
 export default defineConfig((options: Options) => ({
   entry: {
     index: "src/index.tsx",
   },
-  banner: {
-    js: "'use client'",
-  },
-  loader: {
-    ".scss": "css",
-  },
-  esbuildPlugins: [sassPlugin()],
   clean: true,
   format: ["cjs", "esm"],
   external: ["react"],
