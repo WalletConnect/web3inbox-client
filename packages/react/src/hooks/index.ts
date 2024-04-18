efaaba38f2e9be495521d65e90243d5f72592960
Celine Sarafa <celinesarafa@gmail.com>
2024-04-18 16:42:53 +0200

    chore: expose viarecaps in index

--- packages/react/src/hooks/index.ts
+++ packages/react/src/hooks/index.ts
@@ -2,6 +2,7 @@ export * from "./useAllSubscriptions";
 export * from "./useNotificationTypes";
 export * from "./useNotifications";
 export * from "./usePrepareRegistration";
+export * from "./usePrepareRegistrationViaRecaps";
 export * from "./useRegister";
 export * from "./useSubscribe";
 export * from "./useSubscription";
